0197a62bc2841155c8d49dc24e2f90a04de6116b
ageron joachim <ageron.joachim@gmail.com>
2025-10-31 16:24:21 +0100

    ✨ Add Question entity to Game module imports
    - Includes Question in the list of entities for TypeOrmModule.
    - Ensures proper integration of Question in the Game module.

--- apps/backend/src/game/game.module.ts
+++ apps/backend/src/game/game.module.ts
@@ -8,6 +8,7 @@ import {
   Round,
   PlayerSecret,
   Character,
+  Question,
 } from '../database/entities';
 import { GameController } from './game.controller';
 import { GameService } from './game.service';
@@ -23,6 +24,7 @@ import { GameGateway } from './game.gateway';
       Round,
       PlayerSecret,
       Character,
+      Question
     ]),
   ],
   controllers: [GameController],
